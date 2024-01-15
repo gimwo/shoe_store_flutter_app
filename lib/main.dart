25a2b686ce81a198c018f3172baff17db48d851f
mig.alviar45 <mig.alviar45@gmail.com>
2024-01-15 13:17:07 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/cart_provider.dart';
+import 'package:shop_app_flutter/utils/global_variables.dart';
+import 'package:shop_app_flutter/pages/home_page.dart';
+import 'package:shop_app_flutter/pages/product_details_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//Provider
+//ChangeNotifierProvider
+//FutureProvider
+//StreamProvider
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => CartProvider())],
+      child: MaterialApp(
+        title: 'Shopping App',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Color.fromRGBO(254, 206, 1, 1),
+            primary: Color.fromRGBO(254, 206, 1, 1),
+          ),
+          appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            bodySmall: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          useMaterial3: true,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
